450dd819f078a83fb21652fd126669cd1d0b3a81
LionelSocrate <lionelsocrate7@gmail.com>
2023-08-16 16:45:00 +0300

    add base files

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="contenair">
+        <nav>
+          <h1>F2SMI</h1>
+          <div className="links">
+
+          <a href="">home</a>
+          <a href="">contact</a>
+          </div>
+        </nav>
+        <input type="text" />
+      </div>
+    </>
+  );
+}
+
+export default App;
