a0fc8578365b94bcc0cc3128714a5d32ff2f60dd
LionelSocrate <lionelsocrate7@gmail.com>
2023-08-16 17:19:48 +0300

    socrate 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,8 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="contenair">
-        <nav>
-          <h1>F2SMI</h1>
-          <div className="links">
-
-          <a href="">home</a>
-          <a href="">contact</a>
-          </div>
-        </nav>
-        <input type="text" />
+      <div className="container">
+        
       </div>
     </>
   );
